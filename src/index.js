02f9c802e83b42eeed2e565a00cae3c810e8796b
ledung1974 <ledung1974@gmail.com>
2020-10-29 21:33:03 -0600

    version 1

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+window.$isNewGame = true; //global variable
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
